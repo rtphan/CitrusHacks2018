754324a7ddcadaa20d58e1e6922f39674738caa7
rtphan <rtphan@g.ucla.edu>
2018-04-21 22:09:23 -0700

    Made a second page link
    It just makes the sign-in button redirect to another page.

--- citrus-app/src/App.js
+++ citrus-app/src/App.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
-/*import logo from './logo.svg';*/
+import {Switch, Link, Route} from "react-router-dom";
 import './App.css';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className='app'>
-        <header>
-            <div className='wrapper'>
-              <h1>Citrus Hacks 2018</h1>
-              
+    return <div className='app'>
+        <Switch>
+          <Route exact path={"/"} render={() => (<div>
+            <header>
+                <div className='wrapper'>
+                  <h1>$$ Plannerr $$</h1>
+                </div>
+            </header>
+            <div className='container'>
+              <section className='sign-in'>
+                  <form>
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="text" name="password" placeholder="Password" />
+                    <Link to={"/planner-lists"}>
+                      <button>SIGN IN</button>
+                    </Link>
+                  </form>
+              </section>
+              <section className='confirm'>
+                <div className='wrapper'>
+                  <ul>
+                  </ul>
+                </div>
+              </section>
             </div>
-        </header>
-        <div className='container'>
-          <section className='sign-in'>
-              <form>
-                <input type="text" name="username" placeholder="Username" />
-                <input type="text" name="password" placeholder="Password" />
-                <button>SIGN IN</button>
-              </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-              </ul>
             </div>
-          </section>
-        </div>
+          )}/>
+          <Route exact path={"/planner-lists"} render={() => (
+            <div>
+              Welcome to planner-lists
+            </div>
+          )}/>
+        </Switch>
       </div>
-    );
   }
 }
 export default App;
